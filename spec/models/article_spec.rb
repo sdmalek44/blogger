90b6e3c0bf2825a1d08dae001e0e33afaebbc1d4
Stephen Malek <malek.stephen44@gmail.com>
2018-07-04 14:10:34 -0600

    fix article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describr Article, type: :model do
+describe Article, type: :model do
   describe "validations" do
     it {should validate_presence_of(:title)}
     it {should validate_presence_of(:body)}
