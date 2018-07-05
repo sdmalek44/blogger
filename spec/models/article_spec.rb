8843caf9b674bcdcdc97c1ebccb23d7cfbd059c2
Stephen Malek <malek.stephen44@gmail.com>
2018-07-05 16:26:11 -0600

    add one to many relationship comments articles

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 describe Article, type: :model do
+  describe 'relationships' do
+    it {should have_many(:comments)}
+  end
   describe "validations" do
     it {should validate_presence_of(:title)}
     it {should validate_presence_of(:body)}
-
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
-  it {should belong_to(:article)}
+  describe 'relationships' do
+    it {should belong_to(:article)}
+  end
 end
