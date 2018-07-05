7fbc21b3ae86109cb581e4c457b6ebe0c5bea6ca
Stephen Malek <malek.stephen44@gmail.com>
2018-07-05 17:38:55 -0600

    add test that user can add a comment to show and see it on articles page

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -27,5 +27,23 @@ describe 'user sees one article' do
       expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
     end
+    describe 'they fill in a comment form' do
+      it 'displays the co,ment on the article show' do
+        article = Article.create!(title: "New Title", body: "New Body")
+
+        visit article_path(article)
+        author_name = "ME!"
+        body = "some thoughts"
+
+        fill_in "comment[author_name]", with: "ME!"
+        fill_in "comment[body]", with: "some thoughts"
+        click_on "Submit"
+
+        expect(current_path).to eq(articles_path)
+        expect(page).to have_content("Post a Comment")
+        expect(page).to have_content(author_name)
+        expect(page).to have_content(body)
+      end
+    end
   end
 end
