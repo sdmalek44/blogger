e449dd1781d04f3aa0f0c4ec5ab7f79305e27914
Stephen Malek <malek.stephen44@gmail.com>
2018-07-05 17:01:01 -0600

    write test see comments on show page for article

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -11,5 +11,21 @@ describe 'user sees one article' do
 
       expect(page).to have_content(article.title, article.body)
     end
+    it 'displays information for one article' do
+      article = Article.create!(title: "New Title", body: "New Body")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty coments")
+      comment_2 = article.comments.create(author_name: "you", body: "Codafdfents")
+
+      visit articles_path
+
+      click_link article.title
+
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
+    end
   end
 end
